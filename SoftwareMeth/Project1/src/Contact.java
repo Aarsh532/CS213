528ea145e45fb345f6f9a683c3122d0f54e23b5e
Aarsh532 <aarshdadan532@gmail.com>
2023-09-26 18:37:32 -0400

    Update to EventOrganizer

--- SoftwareMeth/Project1/src/Contact.java
+++ SoftwareMeth/Project1/src/Contact.java
@@ -1,7 +1,14 @@
 public class Contact {
-    //private Department department;
+    private Department department;
     private String email;
 
+    public enum Department{
+        CS,
+        EE,
+        ITI,
+        MATH,
+        BAIT
+    }
     public boolean isValid(){
         return true;
     }
