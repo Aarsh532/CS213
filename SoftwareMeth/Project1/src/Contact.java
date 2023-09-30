b6f65dc88cdce1fd9d835b308fc4ecccc11e4a24
hershranpura <hershranpura@gmail.com>
2023-09-30 13:28:08 -0400

    Revert "Revert "Contact""
    This reverts commit bf82899a4b6cf0c5b8732b4799d339f0591084c7.

--- SoftwareMeth/Project1/src/Contact.java
+++ SoftwareMeth/Project1/src/Contact.java
@@ -2,14 +2,43 @@ public class Contact {
     private Department department;
     private String email;
 
-    public enum Department{
-        CS,
-        EE,
-        ITI,
-        MATH,
-        BAIT
+    public Contact(Department department, String email) {
+        this.department = department;
+        this.email = email;
     }
-    public boolean isValid(){
-        return true;
+
+    // Define necessary constants for department acronyms
+    public enum Department {
+        CS, EE, ITI, MATH, BAIT
+    }
+
+    // Check if the contact information is valid
+    public boolean isValid() {
+        return isDepartmentValid(department) && isEmailValid(email);
+    }
+
+    // Helper method to check if the department name is valid
+    private boolean isDepartmentValid(Department department) {
+        for (Department validDepartment : Department.values()) {
+            if (validDepartment == department) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Helper method to check if the email is valid
+    private boolean isEmailValid(String email) {
+        // Check if the email has the @rutgers.edu domain
+        return email.endsWith("@rutgers.edu");
+    }
+
+    // Getters for department and email
+    public Department getDepartment() {
+        return department;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
