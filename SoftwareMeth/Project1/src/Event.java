fcb141856185d7121da348af615d4d75fc9db972
hershranpura <hershranpura@gmail.com>
2023-09-30 13:27:34 -0400

    Revert "Event and Event Calendar"
    This reverts commit 0546733680731cc4ebb0aa94d3f4a80ebce5fdfc.

--- SoftwareMeth/Project1/src/Event.java
+++ SoftwareMeth/Project1/src/Event.java
@@ -1,101 +1,31 @@
-import java.util.Date;
+import java.util.Calendar;
+public class Event{
+    private Date date;
+    private Timeslot startTime;
+    private Location location;
+    private Contact contact;
+    private int duration;
 
-public class Event implements Comparable<Event> {
-    private Date date;             // Event date
-    private Timeslot startTime;    // Starting time
-    private Location location;     // Event location
-    private Contact contact;       // Event contact
-    private int duration;          // Event duration in minutes
-
-    // Constructor
-    public Event(Date date, Timeslot startTime, Location location, Contact contact, int duration) {
-        this.date = date;
-        this.startTime = startTime;
-        this.location = location;
-        this.contact = contact;
-        this.duration = duration;
+    public enum Timeslot{
+        morning,
+        afternoon,
+        evening,
     }
 
-    // Getters and Setters for instance variables
-
-    public Date getDate() {
-        return date;
+    public enum Location{
+        HILL114,
+        ARC103,
+        BE_AUD,
+        TIL232,
+        AB2225,
+        MU302
     }
-
-    public void setDate(Date date) {
+    public void setDate(Date date){
         this.date = date;
     }
 
-    public Timeslot getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Timeslot startTime) {
+    public void setStartTime(Timeslot startTime){
         this.startTime = startTime;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
-    }
-
-    public Contact getContact() {
-        return contact;
-    }
-
-    public void setContact(Contact contact) {
-        this.contact = contact;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    @Override
-    public int compareTo(Event otherEvent) {
-        // Compare events based on date and then startTime if dates are the same
-        int dateComparison = this.date.compareTo(otherEvent.date);
-        if (dateComparison != 0) {
-            return dateComparison;
-        } else {
-            return this.startTime.compareTo(otherEvent.startTime);
-        }
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        Event otherEvent = (Event) obj;
-        return date.equals(otherEvent.date) && startTime.equals(otherEvent.startTime) && location.equals(otherEvent.location);
-    }
-
-
-    @Override
-    public String toString() {
-        return "[Event Date: " + date.toString() + "] [Start: " + startTime.toString() + "] [End: " + calculateEndTime().toString() + "] @" + location.toString() + " [Contact: " + contact.toString() + "]";
-    }
-
-    // Helper method to calculate end time based on duration
-    private Timeslot calculateEndTime() {
-        // Add duration minutes to the start time to get the end time
-        int endHour = startTime.getHour();
-        int endMinute = startTime.getMinute() + duration;
-        if (endMinute >= 60) {
-            endHour += endMinute / 60;
-            endMinute %= 60;
-        }
-        return new Timeslot(endHour, endMinute);
     }
 }
+
--- SoftwareMeth/Project1/src/EventCalender.java
+++ SoftwareMeth/Project1/src/EventCalender.java
@@ -1,140 +1,41 @@
-public class EventCalendar {
-    private Event[] events;       // Array holding the list of events
-    private int numEvents;        // Current number of events in the array
-    private static final int INITIAL_CAPACITY = 4;
-    private static final int CAPACITY_INCREMENT = 4;
-    private static final int NOT_FOUND = -1;
+public class EventCalender {
+    private Event [] events;
+    private int numEvents;
 
-    // Constructor
-    public EventCalendar() {
-        events = new Event[INITIAL_CAPACITY];
-        numEvents = 0;
-    }
-
-    // Private method to find an event in the array
     private int find(Event event) {
-        for (int i = 0; i < numEvents; i++) {
-            if (events[i].equals(event)) {
-                return i;
-            }
-        }
-        return NOT_FOUND;
+        return 0;
     }
 
-    // Private method to increase the capacity of the events array
-    private void grow() {
-        Event[] newEvents = new Event[events.length + CAPACITY_INCREMENT];
-        System.arraycopy(events, 0, newEvents, 0, numEvents);
-        events = newEvents;
+    private void grow(){
+
     }
 
-    // Public method to add an event to the calendar
     public boolean add(Event event) {
-        if (find(event) != NOT_FOUND) {
-            System.out.println("Error: Event with the same date/timeslot/location already exists.");
-            return false;
-        }
 
-        if (numEvents == events.length) {
-            grow();
-        }
-
-        events[numEvents++] = event;
         return true;
     }
 
-    // Public method to remove an event from the calendar
     public boolean remove(Event event) {
-        int index = find(event);
-        if (index == NOT_FOUND) {
-            System.out.println("Error: Event not found in the calendar.");
-            return false;
-        }
-
-        // Shift elements to the left to remove the event
-        for (int i = index; i < numEvents - 1; i++) {
-            events[i] = events[i + 1];
-        }
-
-        numEvents--;
         return true;
     }
 
-    // Public method to check if an event is in the calendar
-    public boolean contains(Event event) {
-        return find(event) != NOT_FOUND;
+    public boolean contains(Event event){
+        return true;
     }
 
-    // Public method to print all events in the calendar
-    public void print() {
-        for (int i = 0; i < numEvents; i++) {
-            System.out.println(events[i].toString());
-        }
+    public void print(){
+
     }
 
-    // Public method to print events sorted by date and timeslot using Bubble Sort
-    public void printByDate() {
-        bubbleSort(new DateTimeslotComparator());
-        print();
+    public void printByDate(){
+
     }
 
-    // Public method to print events sorted by campus and building/room using Bubble Sort
-    public void printByCampus() {
-        bubbleSort(new CampusComparator());
-        print();
-    }
+    public void printByCampus(){
 
-    // Public method to print events sorted by department using Bubble Sort
-    public void printByDepartment() {
-        bubbleSort(new DepartmentComparator());
-        print();
     }
+    public void printByDepartment(){
 
-    // Bubble Sort implementation for sorting events
-    private void bubbleSort(Comparator<Event> comparator) {
-        int n = numEvents;
-        boolean swapped;
-        do {
-            swapped = false;
-            for (int i = 1; i < n; i++) {
-                if (comparator.compare(events[i - 1], events[i]) > 0) {
-                    // Swap events[i-1] and events[i]
-                    Event temp = events[i - 1];
-                    events[i - 1] = events[i];
-                    events[i] = temp;
-                    swapped = true;
-                }
-            }
-            n--;
-        } while (swapped);
-    }
-
-    // Comparator for sorting by date and timeslot
-    private static class DateTimeslotComparator implements Comparator<Event> {
-        @Override
-        public int compare(Event event1, Event event2) {
-            int dateComparison = event1.getDate().compareTo(event2.getDate());
-            if (dateComparison != 0) {
-                return dateComparison;
-            } else {
-                return event1.getStartTime().compareTo(event2.getStartTime());
-            }
-        }
-    }
-
-    // Comparator for sorting by campus and building/room
-    private static class CampusComparator implements Comparator<Event> {
-        @Override
-        public int compare(Event event1, Event event2) {
-            return event1.getLocation().compareTo(event2.getLocation());
-        }
-    }
-
-    // Comparator for sorting by department
-    private static class DepartmentComparator implements Comparator<Event> {
-        @Override
-        public int compare(Event event1, Event event2) {
-            return event1.getContact().getDepartment().compareTo(event2.getContact().getDepartment());
-        }
     }
 }
+
